ee73e94257fe2a15a302a0500830a580af386cce
Giuseppe Rosa <giusepper11@gmail.com>
2020-02-10 13:26:18 -0300

    12049 - Corrigindo data de atualização, conforme regra do CIEDU

--- webapp/fila_da_creche/queries/dt_atualizacao.py
+++ webapp/fila_da_creche/queries/dt_atualizacao.py
@@ -4,7 +4,7 @@ from utils.db_fila_creche_connection import FilaDBConnection
 def get_dt_atualizacao():
     connection = FilaDBConnection()
     querie_atualizacao = f"""
-            SELECT to_timestamp(concat((current_date - 1), ' 06:00:00'), 'YYYY/MM/DD HH') as updated_at
+            SELECT to_timestamp(concat((current_date - 1), ' 09:00:00'), 'YYYY/MM/DD HH') as updated_at
     """
     dt_atualizacao = connection.querie(querie_atualizacao)
     return dt_atualizacao['results'][0]["updated_at"]

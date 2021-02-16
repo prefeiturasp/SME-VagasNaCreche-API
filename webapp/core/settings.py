547d90be9e444d762c5490aeae716c2d96301995
Eduardo <eduardo.bufaino@amcom.com.br>
2021-02-16 16:04:38 -0300

    ALteração de variaveis REDIS

--- webapp/core/settings.py
+++ webapp/core/settings.py
@@ -145,7 +145,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
